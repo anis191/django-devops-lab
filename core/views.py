a82271ff3eeb61c09fbc6303fdd5039e150dc4aa
anisul alam <anisulalam2002@gmail.com>
2026-03-18 01:16:31 +0600

    feat: add Redis caching for posts API with external API integration and performance timing

---
+++ core/views.py
@@ -0,0 +1,29 @@
+import time
+import requests
+from django.core.cache import cache
+from django.http import JsonResponse
+
+def fetch_posts(request):
+    cache_key = "posts_data"
+    ttl = 120 # cache duration in second
+
+    start_time = time.time()
+    data = cache.get(cache_key)
+
+    if data:
+        source = "redis"
+    else:
+        response = requests.get("https://jsonplaceholder.typicode.com/posts")
+        data = response.json()
+        cache.set(cache_key, data, timeout=ttl)
+        source = "api"
+    
+    end_time = time.time()
+    time_taken_ms = round((end_time - start_time) * 1000, 2)
+
+    return JsonResponse({
+        "source" : source,
+        "count" : len(data),
+        "time_taken" : f"{time_taken_ms} ms",
+        "data" : data
+    })
