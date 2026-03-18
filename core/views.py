f9b1f771f9a8c150ff246372abac8c9ebac62be0
anisul alam <anisulalam2002@gmail.com>
2026-03-18 18:01:38 +0600

    debug response

--- core/views.py
+++ core/views.py
@@ -2,7 +2,12 @@ import time
 import requests
 from django.core.cache import cache
 from django.http import JsonResponse
+from django.http import HttpResponse
 
+def fetch_posts(request):
+    return HttpResponse("Hello from fetch_posts! Cache is working if you see this twice quickly.")
+
+"""
 def fetch_posts(request):
     cache_key = "posts_data"
     ttl = 120 # cache duration in second
@@ -27,3 +32,4 @@ def fetch_posts(request):
         "time_taken" : f"{time_taken_ms} ms",
         "data" : data
     })
+"""
