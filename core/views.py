6c55c2982e2646cdb86e0a1351e4d712651026d0
anisul alam <anisulalam2002@gmail.com>
2026-03-19 14:22:55 +0600

    Solved the bug

--- core/views.py
+++ core/views.py
@@ -4,10 +4,10 @@ from django.core.cache import cache
 from django.http import JsonResponse
 from django.http import HttpResponse
 
-def fetch_posts(request):
-    return HttpResponse("Hello from fetch_posts! Cache is working if you see this twice quickly.")
+# def fetch_posts(request):
+    # return HttpResponse("Hello from fetch_posts! Cache is working if you see this twice quickly.")
 
-"""
+
 def fetch_posts(request):
     cache_key = "posts_data"
     ttl = 120 # cache duration in second
@@ -32,4 +32,3 @@ def fetch_posts(request):
         "time_taken" : f"{time_taken_ms} ms",
         "data" : data
     })
-"""
