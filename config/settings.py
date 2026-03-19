eab6cd1b9b0c66056b52200c89912c63fd18b4c0
anisul alam <anisulalam2002@gmail.com>
2026-03-19 14:15:36 +0600

    Fix ALLOWED_HOSTS for Render (Bad Request 400)

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-!x)d6xahltd3wp!vxvvji$9sno(m!n4%l9!4f_7x(m@sz*d6gg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
